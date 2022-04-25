7a2ec56441687b89b6cc80dcb8d3a6944fb058ae
Preeti Yuankrathok <preetisatit@gmail.com>
2022-04-26 01:41:53 +0700

    Drafted index + add social buttons

---
+++ packages/web/src/lib/social/Social.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let icon: string
+  export let color: string
+  export let href: string
+</script>
+
+<a
+  {href}
+  target="_blank"
+  class="bg-white text-gray-500 hoverable rounded-full cursor-pointer px-2 py-1"
+  style:--hover-color={color}
+>
+  <i class="fab fa-{icon} fa-lg" />
+</a>
+
+<style>
+  .hoverable:hover {
+    color: var(--hover-color);
+  }
+</style>
--- packages/web/src/routes/index.svelte
+++ packages/web/src/routes/index.svelte
@@ -1,7 +1,23 @@
+<script lang="ts">
+  import Social from '$lib/social/Social.svelte'
+</script>
+
 <svelte:head>
   <title>Home</title>
 </svelte:head>
 
 <section>
-  hi
+  <div class="p-12 max-w bg-primary rounded-lg shadow-lg text-white flex flex-row">
+    <div class="basis-1/3 px-5">test</div>
+    <div class="basis-2/3 px-5 text-center">
+      <p class="text-5xl font-semibold">Preeti Yuankrathok</p>
+      <p class="mt-3">Software Developer</p>
+
+      <div class="flex flex-row justify-center gap-2 mt-5">
+        <Social icon="linkedin" color="#0077B5" href="https://linkedin.com/in/earthpyy" />
+        <Social icon="github" color="#171515" href="https://github.com/earthpyy" />
+        <Social icon="gitlab" color="#FC6D26" href="https://gitlab.com/earthpyy" />
+      </div>
+    </div>
+  </div>
 </section>
