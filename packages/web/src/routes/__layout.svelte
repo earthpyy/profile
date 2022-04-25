e650b25e02c2035d0a967a4a3d7ceda3964cb7ec
Preeti Yuankrathok <preetisatit@gmail.com>
2022-04-25 16:32:25 +0700

    Init svelte

---
+++ packages/web/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Header from '$lib/header/Header.svelte'
+  import '../app.css'
+  import 'flowbite/dist/flowbite.css'
+</script>
+
+<header>
+  <Header />
+</header>
+
+<main>
+  <slot />
+</main>
---
+++ packages/web/src/routes/index.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<section>
+  hi
+</section>
