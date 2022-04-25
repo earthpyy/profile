9496f7590fb3cb0d5729fbc4edc4d022b4b457a8
Preeti Yuankrathok <preetisatit@gmail.com>
2022-04-25 19:53:58 +0700

    Reuse navitem

--- packages/web/src/lib/header/Header.svelte
+++ packages/web/src/lib/header/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { page } from '$app/stores'
   import { darkMode } from '../../app.stores'
+  import NavItem from './NavItem.svelte'
 
   let menuOpened = false
 
@@ -19,30 +19,9 @@
     </button>
     <div class="w-full md:block md:w-auto md:mr-4" class:hidden={!menuOpened}>
       <ul class="flex flex-col mt-4 md:flex-row md:items-center md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-        <li>
-          <a
-            href="#"
-            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Resume
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Contact
-          </a>
-        </li>
+        <NavItem name="Home" pathname="/" />
+        <NavItem name="Resume" pathname="/resume" />
+        <NavItem name="Contact" pathname="/contact" />
       </ul>
     </div>
 
---
+++ packages/web/src/lib/header/NavItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  export let name: string
+  export let pathname: string
+</script>
+
+<li>
+  <a
+    sveltekit:prefetch
+    href={pathname}
+    class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    class:text-gray-700={$page.url.pathname !== pathname}
+    class:text-primary={$page.url.pathname === pathname}
+  >
+    {name}
+  </a>
+</li>
