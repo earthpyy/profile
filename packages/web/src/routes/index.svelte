4805f947328c48d8468414eec9bb6aaa22c02cd4
Preeti Yuankrathok <preetisatit@gmail.com>
2022-04-26 01:45:25 +0700

    Subtitle semibold

--- packages/web/src/routes/index.svelte
+++ packages/web/src/routes/index.svelte
@@ -11,7 +11,7 @@
     <div class="basis-1/3 px-5">test</div>
     <div class="basis-2/3 px-5 text-center">
       <p class="text-5xl font-semibold">Preeti Yuankrathok</p>
-      <p class="mt-3">Software Developer</p>
+      <p class="mt-3 font-semibold">Software Developer</p>
 
       <div class="flex flex-row justify-center gap-2 mt-5">
         <Social icon="linkedin" alt="LinkedIn" color="#0077B5" href="https://linkedin.com/in/earthpyy" />
