76ec4d3c493b55f2f076e96e252bf699e9584404
Preeti Yuankrathok <preetisatit@gmail.com>
2022-04-25 19:55:54 +0700

    Better color in dark mode

--- packages/web/src/lib/header/NavItem.svelte
+++ packages/web/src/lib/header/NavItem.svelte
@@ -9,9 +9,11 @@
   <a
     sveltekit:prefetch
     href={pathname}
-    class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
     class:text-gray-700={$page.url.pathname !== pathname}
     class:text-primary={$page.url.pathname === pathname}
+    class:dark:text-gray-400={$page.url.pathname !== pathname}
+    class:dark:text-white={$page.url.pathname === pathname}
   >
     {name}
   </a>
